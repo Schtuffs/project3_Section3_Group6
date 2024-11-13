adee0cadaad4d8f103e48c7f0ad23b1938786cde
Schtuffs <kwagler7621@gmail.com>
2024-11-13 14:25:19 -0500

    removed testing code

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,6 +5,6 @@ import Devices.*;
 
 public class Main {
     public static void main(String[] args) {
-        CoffeeMachine coffee = new CoffeeMachine(LocalTime.parse("08:45:00"), "espresso", new String[0], new double[0]);
+        
     }
 }
