4fec05c71f8c8ba6fbd88611212dbb2370b37665
Digi-Bomb <blippert6217@conestogac.on.ca>
2024-11-14 00:56:15 -0500

    Camera and blinds initial implementation
    barebones code for camera and blinds implementation

--- src/Devices/Blinds.java
+++ src/Devices/Blinds.java
@@ -1,17 +1,52 @@
 package Devices;
 
 import java.time.LocalTime;
+//import static java.time.temporal.ChronoUnit.NANOS;
 
 public class Blinds extends Device {
     private boolean isOpen;
-    private LocalTime openTime, closeTime;
+    private static LocalTime openTime;
+    private static LocalTime closeTime;
 
-    public Blinds(LocalTime oTime, LocalTime cTime) {}
+    public Blinds(LocalTime oTime, LocalTime cTime) {
+
+      
+        setCloseTime(cTime);
+        setOpenTime(oTime);
+    }
     
     // Inherited methods
-    public STATES Check() { return STATES.GOOD; }
+    public STATES Check() { 
+        LocalTime current = LocalTime.now();
+        current = current.minusNanos(current.getNano());
+
+        if (current.equals(Blinds.openTime) && !this.isOpen) {
+            // If not on, turn on
+            if (!this.isOpen) {
+                return this.Open();
+            }
+            
+        }
+        
+        return STATES.GOOD; }
     
+        public void setCloseTime(LocalTime cTime){
+            Blinds.closeTime=cTime;
+        }
+
+        public void setOpenTime(LocalTime oTime){
+
+            Blinds.openTime=oTime;
+        }
+
     // Allows remote control of blinds
-    public boolean Open() { return true; }
-    public boolean Close() { return true; }
+    public STATES Open() { 
+        this.isOpen = true;
+        //void public animationFunctionOrSomething();
+        return STATES.GOOD; }
+
+    public STATES Close() { 
+        this.isOpen = false;
+        //void public animationFunctionOrSomething();
+        return STATES.GOOD; }
 }
