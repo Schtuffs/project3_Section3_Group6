ea2c74f048641bf3ff595400544bc400eba3435d
Schtuffs <kwagler7621@gmail.com>
2024-11-19 23:18:43 -0500

    Beginning of the window

--- src/Managers/WindowManager.java
+++ src/Managers/WindowManager.java
@@ -1,18 +1,54 @@
 package Managers;
 
 import javax.swing.*;
+
 import java.util.ArrayList;
 import Devices.*;
 
-public class WindowManager extends JFrame {
-    private int width, height, screen;
+public class WindowManager {
+    // Holds the window size
+    private final int width, height;
+
+    // Holds the device manager to access its variables later
+    private DeviceManager devices;
+
+    // Holds the current screen the window is displaying
+    private JFrame window;
+
+    public enum SCREENS {
+        SCREEN_MAIN, SCREEN_BLINDS, SCREEN_CAMERA, SCREEN_COFFEE, SCREEN_SHOWER, SCREEN_SMOKE, SCREEN_THERMO
+    };
+
+    private SCREENS screen;
     
-    public WindowManager() {}
+    public WindowManager() {
+        // Setup private variables
+        this.width = 1600;
+        this.height = 900;
+        this.screen = SCREENS.SCREEN_MAIN;
+        this.window = new JFrame();
+
+        // Create window
+        this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.window.setSize(this.width, this.height);
+        this.window.setResizable(false);
+        this.window.setVisible(true);
+    }
 
     // Displays a selected screen
-    public void Display(ArrayList<Device> devices) {}
+    public void Display() {
+        
+    }
 
     // Selects a screen
     // Returns if screen is valid option
-    public boolean SelectScreen() { return true; }
+    public boolean SelectScreen(SCREENS newScreen) {
+        try {
+            this.screen = newScreen;
+            return true;
+        }
+        catch (Exception e) {
+            return false;
+        }
+    }
 }
