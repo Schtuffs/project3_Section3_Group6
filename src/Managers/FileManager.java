5cdd31c6e478aea36934ae550640e2c55a93f092
pankocatt <dinalailamak@gmail.com>
2024-11-21 19:02:28 -0500

    Filemanager basis
    have to commit this so i can refresh my repo not sure why vscode is being stubborn #so silly!!

--- src/Managers/FileManager.java
+++ src/Managers/FileManager.java
@@ -2,10 +2,18 @@ package Managers;
 
 import java.util.ArrayList;
 import Devices.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FileManager {
     // Read data from file to be turned into devices
-    public static ArrayList<String> Read(String filename) { return new ArrayList<String>(); }
+    public static ArrayList<String> Read(String filename) { 
+        
+
+        
+        return new ArrayList<String>(); 
+    }
 
     // Write a device to a file
     public static boolean Write(String filename, Device device) { return true; }
